a16ad60101d721bca06c1de0adc72c6c2af404cb
soumi guria <soumiguria.cse2022@nsec.ac.in>
2024-01-10 14:48:38 +0530

    Almost done

---
+++ lib/fast_warmup.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class FastWarmup extends StatelessWidget {
+  final String type;
+  final String time;
+  final String level;
+  final String image;
+  const FastWarmup({
+    super.key,
+    required this.type,
+    required this.time,
+    required this.level,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 10.0, right: 10.0),
+      child: Container(
+        width: 170,
+        height: 100,
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.white,
+        ),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Container(
+                width: 60, // Adjust the width of the image container as needed
+                height:
+                    60, // Adjust the height of the image container as needed
+                child: Image.asset(
+                  image,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(width: 8),
+            Column(
+              children: [
+                Text(
+                  type,
+                  style: const TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  time,
+                  style: const TextStyle(
+                    fontSize: 10,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  level,
+                  style: const TextStyle(
+                    fontSize: 10,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/workout.dart
+++ lib/workout.dart
@@ -1,155 +1,154 @@
 import 'package:flutter/material.dart';
 import 'package:pyramid_developers/best_for_you.dart';
+import 'package:pyramid_developers/fast_warmup.dart';
 
 class Workout extends StatelessWidget {
   const Workout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
-            child: Stack(
-              alignment: Alignment.topLeft,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20.0),
-                    child: ShaderMask(
-                      shaderCallback: (Rect bounds) {
-                        return LinearGradient(
-                          begin: Alignment.centerLeft,
-                          end: Alignment.centerRight,
-                          colors: [
-                            Color(0xFF000000),
-                            Color(0xFF000000),
-                          ],
-                        ).createShader(bounds);
-                      },
-                      blendMode: BlendMode.dstIn,
-                      child: Image.asset(
-                        'assets/images/image 2.png',
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
+              child: Stack(
+                alignment: Alignment.topLeft,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: ShaderMask(
+                        shaderCallback: (Rect bounds) {
+                          return LinearGradient(
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
+                            colors: [
+                              Color(0xFF000000),
+                              Color(0xFF000000),
+                            ],
+                          ).createShader(bounds);
+                        },
+                        blendMode: BlendMode.dstIn,
+                        child: Image.asset(
+                          'assets/images/image 2.png',
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const Positioned(
+                  const Positioned(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding:
+                              EdgeInsets.only(top: 25.0, left: 20.0, right: 20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Best Quarantine\nWorkout',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 50),
+                              Row(
+                                children: [
+                                  Text(
+                                    'See more',
+                                    style: TextStyle(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xFFBBF246),
+                                    ),
+                                  ),
+                                  Icon(
+                                    Icons.chevron_right,
+                                    color: Color(0xFFBBF246),
+                                    size: 20,
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 25.0, left: 20.0, right: 20.0),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Best for you',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  )),
+            ),
+            Container(
+                height: 230,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
                   child: Column(
                     children: [
-                      Padding(
-                        padding:
-                            EdgeInsets.only(top: 25.0, left: 20.0, right: 20.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Best Quarantine\nWorkout',
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            SizedBox(height: 50),
-                            Row(
-                              children: [
-                                Text(
-                                  'See more',
-                                  style: TextStyle(
-                                    fontSize: 10,
-                                    fontWeight: FontWeight.bold,
-                                    color: Color(0xFFBBF246),
-                                  ),
-                                ),
-                                Icon(
-                                  Icons.chevron_right,
-                                  color: Color(0xFFBBF246),
-                                  size: 20,
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
+                      Row(
+                        children: [
+                          BestForYou(
+                              image: 'assets/images/image63.png',
+                              type: 'Belly fat burner',
+                              time: '10 min',
+                              level: 'Beginner'),
+                          BestForYou(
+                              image: 'assets/images/image65.png',
+                              type: 'Lose Fat',
+                              time: '10 min',
+                              level: 'Beginner'),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          BestForYou(
+                              image: 'assets/images/Image.png',
+                              type: 'Plank',
+                              time: '10 min',
+                              level: 'Beginner'),
+                          BestForYou(
+                              image: 'assets/images/Image (1).png',
+                              type: 'Build Whider E',
+                              time: '10 min',
+                              level: 'Beginner'),
+                        ],
                       ),
                     ],
                   ),
-                ),
-              ],
+                )),
+            const Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 10),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Challenge',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  )),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(top: 25.0, left: 20.0, right: 20.0),
-            child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Best for you',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                )),
-          ),
-          Container(
-              height: 230,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        BestForYou(
-                            image: 'assets/images/image63.png',
-                            type: 'Belly fat burner',
-                            time: '10 min',
-                            level: 'Beginner'),
-                        BestForYou(
-                            image: 'assets/images/image65.png',
-                            type: 'Lose Fat',
-                            time: '10 min',
-                            level: 'Beginner'),
-                      ],
-                    ),
-                    SizedBox(height: 10),
-                    Row(
-                      children: [
-                        BestForYou(
-                            image: 'assets/images/Image.png',
-                            type: 'Plank',
-                            time: '10 min',
-                            level: 'Beginner'),
-                        BestForYou(
-                            image: 'assets/images/Image (1).png',
-                            type: 'Build Whider E',
-                            time: '10 min',
-                            level: 'Beginner'),
-                      ],
-                    ),
-                  ],
-                ),
-              )),
-          const Padding(
-            padding: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 10),
-            child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Challenge',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                )),
-          ),
-          Container(
-              height: 230,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
+            Container(
+                height: 120,
                 child: Column(
                   children: [
                     SingleChildScrollView(
                       scrollDirection: Axis.horizontal,
                       child: Padding(
-                        padding: EdgeInsets.only(
-                            left: 20.0, right: 20.0, bottom: 10),
+                        padding: EdgeInsets.only(left: 20.0, right: 20.0),
                         child: Row(
                           children: [
                             Container(
@@ -182,12 +181,10 @@ class Workout extends StatelessWidget {
                                 alignment: Alignment.bottomLeft,
                                 child: Padding(
                                   padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    'Sprint\nChallenge',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                    )
-                                  ),
+                                  child: Text('Sprint\nChallenge',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      )),
                                 ),
                               ),
                             ),
@@ -253,9 +250,81 @@ class Workout extends StatelessWidget {
                       ),
                     ),
                   ],
+                )),
+            const Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 10),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Fast Warmup',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  )),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        FastWarmup(
+                          image: 'assets/images/Image.png',
+                          type: 'Leg Exercise',
+                          time: '10 min',
+                          level: 'Beginner',
+                        ),
+                        FastWarmup(
+                          image: 'assets/images/Image.png',
+                          type: 'Backward lun',
+                          time: '5 min',
+                          level: 'Beginner',
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        FastWarmup(
+                          image: 'assets/images/Image.png',
+                          type: 'Leg Exercise',
+                          time: '10 min',
+                          level: 'Beginner',
+                        ),
+                        FastWarmup(
+                          image: 'assets/images/Image.png',
+                          type: 'Leg Exercise',
+                          time: '10 min',
+                          level: 'Beginner',
+                        )
+                      ],
+                    ),
+                  ],
                 ),
-              )),
-        ],
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: BottomNavigationBar(
+                items: const [
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.search),
+                    label: 'Search',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.person),
+                    label: 'Profile',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
